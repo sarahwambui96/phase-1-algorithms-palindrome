54545f0e7b78508c58f336bdbc3e87fa4ac51e5c
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-30 00:52:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord =  reverseString(word)
+  if (word === reversedWord){
+    return true;
+  }else {
+    return false;
+  }
+}
+
+function reverseString(word){
+  const wordArray = word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord
 }
 
 /* 
   Add your pseudocode here
+  reverse the word
+  if the reversed word is the same as the actual  word return true
+  if not return false
 */
 
 /*
@@ -13,6 +29,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  //level === level
+  //refer === refer
+  //madam === madam
+  //wow === wow
+  //car ===rac*
+  console,log("Expecting: true")
+  console.log("=>", isPalindrome("level"))
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
